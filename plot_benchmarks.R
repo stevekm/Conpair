7a7f7e217551e116307d02ccbe2e086789ccf176
Stephen Kelly <stevekm@users.noreply.github.com>
2020-08-04 18:24:21 -0400

    add benchmark plot script

---
+++ plot_benchmarks.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# module load R/R-3.6.3
+library("ggplot2")
+df <- read.delim(file = "benchmarks.tsv", sep = '\t')
+colnames(df) <- c("num_threads", "time", "num_pairs", "num_tumors", "num_normals")
+df[["time_per_pair"]] <- df[["time"]] / df[["num_pairs"]]
+df[["num_threads"]] <- factor(df[["num_threads"]], levels = sort(unique(df[["num_threads"]])))
+
+pdf("benchmark_time_pairs_threads.pdf", width = 10)
+ggplot(data = df, aes(x = num_pairs, y = time/60, color = num_threads)) + 
+    geom_line() +
+    geom_point() + 
+    theme_bw() +
+    ggtitle("Conpair Execution Time (min) vs. Number of Pairs vs. Thread Count")
+dev.off()
+
+pdf("time_per_pair.pdf")
+ggplot(data = df, aes(x = num_threads, y = time_per_pair)) +
+    geom_boxplot() +
+    ggtitle("Conpair Time Per Pair vs. Thread Count") +
+    theme_bw()
+dev.off()
+
+save.image()
