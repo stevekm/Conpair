7fcfb17694e91a2d31851abceade21d15fe82fd1
Stephen Kelly <stevekm@users.noreply.github.com>
2020-08-05 17:41:20 -0400

    add aggregate table output to plot script

--- plot_benchmarks.R
+++ plot_benchmarks.R
@@ -6,9 +6,9 @@ colnames(df) <- c("num_threads", "time", "num_pairs", "num_tumors", "num_normals
 df[["time_per_pair"]] <- df[["time"]] / df[["num_pairs"]]
 df[["num_threads"]] <- factor(df[["num_threads"]], levels = sort(unique(df[["num_threads"]])))
 
-pdf("benchmark_time_pairs_threads.pdf", width = 10)
-ggplot(data = df, aes(x = num_pairs, y = time/60, color = num_threads, fill = action)) + 
-    geom_point() + 
+pdf("time_pairs_threads.pdf", width = 10)
+ggplot(data = df, aes(x = num_pairs, y = time/60, color = num_threads, fill = action)) +
+    geom_point() +
     geom_smooth(method = 'lm') +
     theme_bw() +
     ggtitle("Conpair Execution Time (min) vs. Number of Pairs vs. Thread Count")
@@ -20,5 +20,14 @@ ggplot(data = df, aes(x = num_threads, y = time_per_pair)) +
     ggtitle("Conpair Execution Time Per Pair (sec) vs. Thread Count") +
     theme_bw()
 dev.off()
+
+aggr <- aggregate(time_per_pair~num_threads, data = df, mean)
+colnames(aggr) <- c("num_threads", "mean")
+aggr[["median"]] <- aggregate(time_per_pair~num_threads, data = df, median)[["time_per_pair"]]
+aggr[["min"]] <- aggregate(time_per_pair~num_threads, data = df, min)[["time_per_pair"]]
+aggr[["max"]] <- aggregate(time_per_pair~num_threads, data = df, max)[["time_per_pair"]]
+aggr[["sd"]] <- aggregate(time_per_pair~num_threads, data = df, sd)[["time_per_pair"]]
+
+write.table(x = aggr, file = "aggregate_time_per_pair.tsv", quote = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE)
 
 save.image()
