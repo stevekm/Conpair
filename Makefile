972d23e91caf42ce87992607443c18884ebce059
Stephen Kelly <stevekm@users.noreply.github.com>
2020-08-10 10:20:26 -0400

    add subset unit test and update Makefile example paths

--- Makefile
+++ Makefile
@@ -35,39 +35,13 @@ conda:
 install: conda
 	pip install scipy==1.1.0 numpy==1.15.4
 
-# python ../Conpair/scripts/verify_concordances.py -p pairing.txt -N ... -T ...
-# python ../Conpair/scripts/estimate_tumor_normal_contaminations.py -p pairing.txt -N ... -T ...
-# export GATK_JAR=/path/to/gatk.jar
-TUMOR_PILEUP:=$(shell head -1 tumor_pileups.txt)
-NORMAL_PILEUP:=$(shell head -1 normal_pileups.txt)
-test-run1:
-	export CONPAIR_DIR=$(CURDIR)
-	python2.7 scripts/verify_concordance.py \
-	--tumor_pileup $(TUMOR_PILEUP) \
-	--normal_pileup $(NORMAL_PILEUP) && \
-	python2.7 scripts/estimate_tumor_normal_contamination.py \
-	--tumor_pileup $(TUMOR_PILEUP) \
-	--normal_pileup $(NORMAL_PILEUP)
-# 0.782
-# Based on 188/7387 markers (coverage per marker threshold: 10 reads)
-# Minimum mappinq quality: 10
-# Minimum base quality: 20
-# Normal sample contamination level: 0.113%
-# Tumor sample contamination level: 0.0%
-
-test-run2:
-	export CONPAIR_DIR=$(CURDIR)
-	python2.7 scripts/verify_concordance2.py \
-	--tumor_pileup $(TUMOR_PILEUP) \
-	--normal_pileup $(NORMAL_PILEUP)
-
 THREADS:=8
 NUM_TUMORS:=1
 NUM_NORMALS:=5
 ACTIONS:=concordance,contamination
-TUMOR_FILE:=tumor_pileups.txt
-NORMAL_FILE:=normal_pileups.txt
-MARKERS:=data/markers/GRCh37.autosomes.phase3_shapeit2_mvncall_integrated.20130502.SNV.genotype.sselect_v4_MAF_0.4_LD_0.8.txt
+TUMOR_FILE:=tumors.txt
+NORMAL_FILE:=normals.txt
+MARKERS:=/juno/work/ci/kellys5/projects/conpair-dev/markers/IMPACT468/FP_tiling_genotypes_for_Conpair.txt
 ARGS:=
 run:
 	python run.py \
@@ -92,3 +66,33 @@ submit: $(SUB_THREADS)
 
 bash:
 	bash
+
+test:
+	python modules/test_concordance.py
+
+# ~~~~~ #
+# python ../Conpair/scripts/verify_concordances.py -p pairing.txt -N ... -T ...
+# python ../Conpair/scripts/estimate_tumor_normal_contaminations.py -p pairing.txt -N ... -T ...
+# export GATK_JAR=/path/to/gatk.jar
+TUMOR_PILEUP:=$(shell head -1 $(TUMOR_FILE))
+NORMAL_PILEUP:=$(shell head -1 $(NORMAL_FILE))
+test-run1:
+	export CONPAIR_DIR=$(CURDIR)
+	python2.7 scripts/verify_concordance.py \
+	--tumor_pileup $(TUMOR_PILEUP) \
+	--normal_pileup $(NORMAL_PILEUP) && \
+	python2.7 scripts/estimate_tumor_normal_contamination.py \
+	--tumor_pileup $(TUMOR_PILEUP) \
+	--normal_pileup $(NORMAL_PILEUP)
+# 0.782
+# Based on 188/7387 markers (coverage per marker threshold: 10 reads)
+# Minimum mappinq quality: 10
+# Minimum base quality: 20
+# Normal sample contamination level: 0.113%
+# Tumor sample contamination level: 0.0%
+
+test-run2:
+	export CONPAIR_DIR=$(CURDIR)
+	python2.7 scripts/verify_concordance2.py \
+	--tumor_pileup $(TUMOR_PILEUP) \
+	--normal_pileup $(NORMAL_PILEUP)
--- modules/test_concordance.py
+++ modules/test_concordance.py
@@ -7,6 +7,8 @@ import os
 import unittest
 import concordance
 from ContaminationMarker import get_markers
+import tempfile
+import shutil
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.dirname(THIS_DIR)
@@ -16,6 +18,7 @@ marker_file = os.path.join(PARENT_DIR, 'data', 'markers', 'GRCh37.autosomes.phas
 class TestCocordance(unittest.TestCase):
     def test_concordance1(self):
         """
+        Test the concordance of the included pileup tumor and normal files
         """
         tumor_pileup = os.path.join(PILEUP_DIR, 'NA12878_tumor80x.gatk.pileup.txt')
         normal_pileup = os.path.join(PILEUP_DIR, 'NA12878_normal40x.gatk.pileup.txt')
@@ -27,8 +30,9 @@ class TestCocordance(unittest.TestCase):
         self.assertEqual(num_markers_used, 7363)
         self.assertEqual(num_total_markers, 7387)
 
-    def test_concordanc2(self):
+    def test_concordance2(self):
         """
+        Test that the included tumor pileup has 100% concordance to itself
         """
         tumor_pileup = os.path.join(PILEUP_DIR, 'NA12878_tumor80x.gatk.pileup.txt')
         markers_data = get_markers(marker_file)
@@ -38,6 +42,40 @@ class TestCocordance(unittest.TestCase):
         self.assertEqual(concordance_val, 1.0)
         self.assertEqual(num_markers_used, 7374)
         self.assertEqual(num_total_markers, 7387)
+
+    def test_concordance_subset(self):
+        """
+        Check the concordance values of the included example tumor and normal for various numbers of subset entries from the pileup
+        """
+        tumor_pileup = os.path.join(PILEUP_DIR, 'NA12878_tumor80x.gatk.pileup.txt')
+        normal_pileup = os.path.join(PILEUP_DIR, 'NA12878_normal40x.gatk.pileup.txt')
+        markers_data = get_markers(marker_file)
+
+        tmpdirpath = tempfile.mkdtemp()
+        subset_tumor_5000_file = os.path.join(tmpdirpath, "tumor5000.pileup")
+        subset_normal_5000_file = os.path.join(tmpdirpath, "normal5000.pileup")
+        with open(tumor_pileup) as fin, open(subset_tumor_5000_file, 'w') as fout:
+            for i, line in enumerate(fin):
+                if i < 5000:
+                    fout.write(line)
+                else:
+                    break
+        with open(normal_pileup) as fin, open(subset_normal_5000_file, 'w') as fout:
+            for i, line in enumerate(fin):
+                if i < 5000:
+                    fout.write(line)
+                else:
+                    break
+
+        concordance_val, num_markers_used, num_total_markers = concordance.main(tumor_pileup = subset_tumor_5000_file, normal_pileup = subset_normal_5000_file, markers_data = markers_data)
+
+        self.assertEqual(concordance_val, 0.9993995196156925)
+        self.assertEqual(num_markers_used, 4996)
+        self.assertEqual(num_total_markers, 7387)
+
+        shutil.rmtree(tmpdirpath)
+
+
 
 if __name__ == "__main__":
     unittest.main()
